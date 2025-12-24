7168169c410584a67e7790eb5c9f0b9c6a19ce23
mikelindj <mike.lindj@gmail.com>
2025-12-24 11:36:41 +0800

    fixed Footer IMAGE error.

--- app/book/page.tsx
+++ app/book/page.tsx
@@ -592,11 +592,9 @@ export default function BookPage() {
         <div className="container max-w-6xl mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-6">
            <div className="flex items-center gap-3">
                <div className="relative h-10 w-10 opacity-90 hover:opacity-100 transition-opacity duration-500">
-                  <Image 
+                  <img 
                     src="/images/acs-logo.png" 
                     alt="ACS Logo" 
-                    width={40}
-                    height={40}
                     className="object-contain w-full h-full"
                   />
                </div>
